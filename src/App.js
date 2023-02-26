e1e19eba039cdf819ab06eb8b5f326d7926b3ab8
karima <timmermann@gmx.at>
2023-02-26 07:13:05 +0100

    Add main game screen to App.jsx

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import './App.css';
 import Header from "./components/Header";
 import GameLanding from "./components/GameLanding";
 import Footer from "./components/Footer";
+import Leaderboard from "./components/Leaderboard";
+import GameScreen from "./components/GameScreen";
 
 function App() {
   return (
     <div className="App">
-        <div className="main-page">
+        {/* <div className="main-page">
           <Header />
           <GameLanding />
           <Footer />
+        </div>
+        <div className="leaderboard-page">
+            <Leaderboard />
+        </div> */}
+        <div className="game-page">
+            <GameScreen />
         </div>
     </div>
   );
