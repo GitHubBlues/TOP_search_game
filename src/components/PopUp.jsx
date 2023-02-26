96ea22f3ad8b5efa4c9201e01922289c9770890b
karima <timmermann@gmx.at>
2023-02-26 07:11:43 +0100

    React component pop up telling if image was found

---
+++ src/components/PopUp.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './PopUp.css';
+
+export default function PopUp( props ) {
+    // let popupX = `${props.coord[0]}px`;
+    // popupY = `${-1200+props.coord[1]}px`;
+    let popupX = `${-1300+props.coord[0]}px`;
+    let popupY = `${props.coord[1]}px`;
+    console.log("fff", popupX, popupY)
+    console.log(popupX, popupY)
+
+    let popupTxt = props.message1 + " " + props.message2;
+    return (
+        // <div className="popup" style={{ top: {popupY}, left: {popupX}, lineHeight : 10, padding: 40 }} > POP UP
+        <div className="popup" style={{ top: popupY, left: popupX, padding: 0 }} > { popupTxt }
+        </div>
+    )
+}
