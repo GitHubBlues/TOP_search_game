5eecebd58900e42215cb94eb6147827b45461b45
karima <timmermann@gmx.at>
2023-02-27 08:54:44 +0100

    Add links and routes to different pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import './App.css';
+import React, { Component }  from 'react';
 import Header from "./components/Header";
 import GameLanding from "./components/GameLanding";
 import Footer from "./components/Footer";
 import Leaderboard from "./components/Leaderboard";
 import GameScreen from "./components/GameScreen";
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-        {/* <div className="main-page">
-          <Header />
-          <GameLanding />
-          <Footer />
-        </div>
-        <div className="leaderboard-page">
-            <Leaderboard />
-        </div> */}
-        <div className="game-page">
-            <GameScreen />
-        </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* <div className="App"> */}
+        <Route path="/leaderboard" element={    
+            <div className="leaderboard-page">
+                <Leaderboard />
+            </div>
+        }/>    
+        <Route path="/game" element={    
+            <div className="game-page">
+                <GameScreen />
+            </div>
+        }/> 
+        <Route path="/" element={  
+            <div className="main-page">
+              <Header />
+              <GameLanding />
+              <Footer />
+            </div>
+        }/>     
+        {/* </div>     */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/GameLanding.jsx
+++ src/components/GameLanding.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-// import { collection, getDocs } from "firebase/firestore";
-// import { db } from "../lib/init-firebase.js";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../lib/init-firebase.js";
+import { Link } from "react-router-dom";
 import './GameLanding.css'
 
 import picture1 from '../assets/Motiv1-Baustelle-Badesee1600x1200px.jpg';
-import picture2 from '../assets/Motiv1-Baustelle-Badesee1600x1200px.jpg';
-import picture3 from '../assets/Motiv1-Baustelle-Badesee1600x1200px.jpg';
+import picture2 from '../assets/Motiv2-Zoo-Flohmarkt-1600x1200px.jpg';
+import picture3 from '../assets/Motiv3-Bauernhof-Wiesenfest-1600x1200px.jpg';
 
 export default function GameLanding() {
 
@@ -14,22 +15,32 @@ export default function GameLanding() {
         <p className="instructions">Find 6 hidden objects in each picture. And be fast, so you reach the first place in the leaderboard.<br />Good luck!</p>
         
         <div className="three-image-container">
+            
             <div className="space"></div>
-            <div className="image-xs-container">
-                <img className="image-xs" src={picture1} alt="lake in summer" />
-                <div className="image-name">At the lake</div>
-            </div>
+                <Link style={{textDecoration: 'none'}} to="/game" state={{ imageNr: 0 }} >
+                    <div className="image-xs-container">
+                        <img className="image-xs" src={picture1} alt="lake in summer" />
+                        <div className="image-name">At the lake</div>
+                    </div>    
+                </Link>
             <div className="space"></div>
-            <div className="image-xs-container">
-                <img className="image-xs" src={picture2} alt="zoo" />
-                <div className="image-name">At the zoo</div>
-            </div>
+            
+            <Link style={{textDecoration: 'none'}} to="/game" state={{ imageNr: 1 }} >
+                <div className="image-xs-container">
+                    <img className="image-xs" src={picture2} alt="zoo" />
+                    <div className="image-name">At the zoo</div>
+                </div>
+            </Link>    
             <div className="space"></div>
-            <div className="image-xs-container">
-                <img className="image-xs" src={picture3} alt="farm" />
-                <div className="image-name">At the farm</div>
-            </div>
+            
+            <Link style={{textDecoration: 'none'}} to="/game" state={{ imageNr: 2 }} >
+                <div className="image-xs-container">
+                    <img className="image-xs" src={picture3} alt="farm" />
+                    <div className="image-name">At the farm</div>
+                </div>    
+            </Link>
             <div className="space"></div>
+
         </div> 
         
     </div>
