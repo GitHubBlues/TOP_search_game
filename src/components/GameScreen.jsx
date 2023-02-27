84bc90d010551eae0c30dd82d1ebad2970679840
karima <timmermann@gmx.at>
2023-02-27 08:55:39 +0100

    Add functionality for dynamic pop up messages

--- src/components/GameScreen.jsx
+++ src/components/GameScreen.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { useLocation } from 'react-router-dom'
 import './GameScreen.css'
 import generateGameData from "./generateGameData";
 import GameSidebar from './GameSidebar'
@@ -7,12 +8,17 @@ import PopUp from './PopUp'
 
 
 export default function GameScreen() {
+    const location = useLocation()
+    const { imageNr } = location.state
+    
+    console.log(imageNr)
+
     const initialDB = generateGameData();
-    let imageNr = 0;
+    //let imageNr = 0;
     let tmp
-    if (imageNr == 0){
+    if (imageNr === 0){
         tmp = initialDB[0].image1;
-    } else if (imageNr == 1){
+    } else if (imageNr === 1){
         tmp = initialDB[1].image2;
     } else {
         tmp = initialDB[2].image3;
@@ -21,16 +27,18 @@ export default function GameScreen() {
     const [toSearch, setToSearch] = useState( tmp.items )
     const [modal, setModal] = useState( false )
     const [clickXY, setClickXY] = useState( [] ) 
+    const [message, setMessage] = useState( ["Unsuccsessful attempt"] ) 
     
     function checkTargetFound( [playerX, playerY]) {
         let image = document.querySelector('.image-xl');
-        let updatedToSearch = JSON.parse(JSON.stringify(toSearch))
+        let updatedToSearch = JSON.parse(JSON.stringify(toSearch));
+        setMessage( "Unsuccsessful attempt" );
 
         updatedToSearch.forEach( (item) =>{ 
             if ( (Math.abs(item.x/1600 - playerX/image.offsetWidth)<0.023) && (Math.abs(item.y/1200 - playerY/image.offsetHeight)<0.023) ) {
-            console.log("YES", item.name)
-            item.found = true;
-            setToSearch(updatedToSearch)
+                item.found = true;
+                setToSearch(updatedToSearch)
+                setMessage( item.name + " found");
             }
         })
     }
@@ -43,16 +51,17 @@ export default function GameScreen() {
         checkTargetFound( [playerX, playerY] )
         setClickXY([playerX, playerY])
         setModal(true)
+        console.log(clickXY);
    }
 
     return(
         <div className="game">
-             {/*The coord props in the GamsSidebar component are not used. They were added so taht the component re-rendres as planned*/}
+             {/*The coord props in the GamsSidebar component are not used. They were added so that the component re-rendres as planned*/}
             <GameSidebar items = { toSearch } coord={ clickXY }/> 
             <div className = "image-container">
                 <GameImage src={ tmp.src } alt={ tmp.alt } items={ toSearch } handleClick={ getPopUp } />
                 { modal &&
-                <PopUp coord={ clickXY } message1={ clickXY[0] } message2={clickXY[1]}/>
+                <PopUp coord={ clickXY } message={ message } />
                 }
             </div>       
         </div>
--- src/components/PopUp.jsx
+++ src/components/PopUp.jsx
@@ -9,8 +9,9 @@ export default function PopUp( props ) {
     console.log("fff", popupX, popupY)
     console.log(popupX, popupY)
 
-    let popupTxt = props.message1 + " " + props.message2;
-    return (
+    let popupTxt = props.message + " " + popupX+ " " + popupY;
+    console.log(props)
+     return (
         // <div className="popup" style={{ top: {popupY}, left: {popupX}, lineHeight : 10, padding: 40 }} > POP UP
         <div className="popup" style={{ top: popupY, left: popupX, padding: 0 }} > { popupTxt }
         </div>
