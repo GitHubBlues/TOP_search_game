9a773307ce2f3918a93607aab2aa6d4ecea7882f
karima <timmermann@gmx.at>
2023-03-03 15:34:24 +0100

    Add functionality for saving score

--- src/components/GameScreen.jsx
+++ src/components/GameScreen.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react'
-import { useLocation } from 'react-router-dom'
-import './GameScreen.css'
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import generateGameData from "./generateGameData";
-import GameSidebar from './GameSidebar'
-import GameImage from './GameImage'
-import PopUp from './PopUp'
-
+import GameSidebar from './GameSidebar';
+import GameImage from './GameImage';
+import PopUp from './PopUp';
+import SaveScore from './SaveScore';
+import './GameScreen.css';
 
 export default function GameScreen() {
     const location = useLocation()
@@ -26,9 +26,15 @@ export default function GameScreen() {
 
     const [toSearch, setToSearch] = useState( tmp.items )
     const [modal, setModal] = useState( false )
+    const [enterName, setEnterName] = useState( false )
     const [clickXY, setClickXY] = useState( [] ) 
     const [message, setMessage] = useState( ["Unsuccsessful attempt"] ) 
-    
+    const [startTimer, setStartTimer] = useState( Date.now() ) 
+    const [elapsedTime, setElapsedTime] = useState( Date.now() ) 
+    console.log(startTimer);
+    // const timerStart = Date.now();
+    // console.log("timerstart", timerStart);
+   
     function checkTargetFound( [playerX, playerY]) {
         let image = document.querySelector('.image-xl');
         let updatedToSearch = JSON.parse(JSON.stringify(toSearch));
@@ -40,19 +46,61 @@ export default function GameScreen() {
                 setToSearch(updatedToSearch)
                 setMessage( item.name + " found");
             }
+            console.log(item.x/1600, playerX/image.offsetWidth, playerX, image.offsetWidth)
+            console.log(item.y/1200, playerY/image.offsetHeight, playerY, image.offsetHeight)
         })
+
     }
 
     function getPopUp(e) {
         // 200px are subtracted below accounting for the sideebar
-        let playerX = e.pageX-200;
+        let playerX = e.pageX - 200;
         let playerY = e.pageY;
         
         checkTargetFound( [playerX, playerY] )
         setClickXY([playerX, playerY])
         setModal(true)
-        console.log(clickXY);
-   }
+        // console.log(clickXY);
+        // console.log("rajacounte0")
+        console.log("rajacounte1")
+    }
+
+    useEffect(() => {
+        let counter = 0;
+        toSearch.forEach( (item) =>{
+             if (item.found==true) counter=counter+1;
+             console.log("inside", item.name, counter)
+             console.log(Date.now())
+        })  
+         if ( counter==6 ) {
+             console.log("queonda")
+             setEnterName(true)
+             console.log("entername ", enterName)
+             const timerEnd = Date.now() ;
+             setElapsedTime((timerEnd-startTimer)/1000);
+             console.log(timerEnd-startTimer)
+        }; 
+    }, [toSearch]);
+
+
+    // function testEndOfGame() {
+    //     console.log(toSearch)
+    //     let counter = 0;
+    //     toSearch.forEach( (item) =>{
+    //         if (item.found==true) counter=counter+1;
+    //         console.log("inside", item.name, counter)
+    //     })
+    //     console.log("rajacounte", counter)
+    //     if ( counter==6 ) {
+    //         console.log("queonda")
+    //         setEnterName(true)
+    //         console.log("entername ", enterName)
+    //     };   
+    // }
+
+    function enterScore() {
+
+    }
 
     return(
         <div className="game">
@@ -63,7 +111,10 @@ export default function GameScreen() {
                 { modal &&
                 <PopUp coord={ clickXY } message={ message } />
                 }
-            </div>       
+            </div>    
+                { enterName &&
+                <SaveScore timer={ elapsedTime } image={ imageNr } />
+                }   
         </div>
     )
 }
