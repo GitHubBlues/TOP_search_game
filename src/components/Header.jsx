bc3551f7004c20d80252a0eba2528a0a38ac7f5a
karima <timmermann@gmx.at>
2023-03-03 15:37:34 +0100

    Add link to leaderboard page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 import './Header.css'
 
 export default function Header() {
   return (
     <div className="header" >
         <h1 className="title">Search and find</h1>
-        <button type="button">Leaderboard</button> 
+        <Link style={{textDecoration: 'none'}} to="/leaderboard" >
+          <button type="button">Leaderboard</button> 
+        </Link>
     </div>
   )
 }
