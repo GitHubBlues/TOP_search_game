5f01e324db04bbeadf00701c2801c28dbcd84b15
karima <timmermann@gmx.at>
2023-03-03 15:28:44 +0100

    Fixed popup positioning and styling

--- src/components/PopUp.jsx
+++ src/components/PopUp.jsx
@@ -4,16 +4,18 @@ import './PopUp.css';
 export default function PopUp( props ) {
     // let popupX = `${props.coord[0]}px`;
     // popupY = `${-1200+props.coord[1]}px`;
-    let popupX = `${-1300+props.coord[0]}px`;
+    let popupX = `${-200+props.coord[0]}px`;
     let popupY = `${props.coord[1]}px`;
     console.log("fff", popupX, popupY)
     console.log(popupX, popupY)
 
     let popupTxt = props.message + " " + popupX+ " " + popupY;
     console.log(props)
-     return (
+
+    return (
         // <div className="popup" style={{ top: {popupY}, left: {popupX}, lineHeight : 10, padding: 40 }} > POP UP
-        <div className="popup" style={{ top: popupY, left: popupX, padding: 0 }} > { popupTxt }
+        <div className="popup" style={{ top: popupY, left: popupX }} > 
+           { popupTxt }
         </div>
     )
 }
